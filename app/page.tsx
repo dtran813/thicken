f91c2b5cab55e2178ec4d744f0f9960b7d0f02fb
dtran <dtran13899@gmail.com>
2023-05-27 14:40:46 -0600

    First HTML content and installed prettier-plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,129 @@
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="bg-yellow-300 p-5 text-center ">
         <h1>Thicken Thighs</h1>
       </header>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Specials</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <section>
+        <h2>
+          Welcome to Thicken Thighs: Where Chickens Are Our Slow-Roasted
+          Superstars!
+        </h2>
+
+        <p>
+          At Thicken Thighs, we take pride in being the premier slow food
+          restaurant in the industry. Our commitment to slowness and
+          chicken-centric cuisine sets us apart. With a touch of sarcasm and a
+          dash of humor, we&apos;re here to serve up a memorable dining
+          experience that will leave you clucking for more!
+        </p>
+
+        <h2>Our Commitment to Slowness</h2>
+
+        <p>
+          We&apos;ve mastered the art of slow cooking, taking our time to craft
+          dishes that burst with amazing flavors. We&apos;re not just slow,
+          we&apos;re award-winningly slow! Join us on a culinary adventure where
+          every bite is worth the wait.
+        </p>
+
+        <h2>Chicken Reproduction Fund</h2>
+
+        <p>
+          At Thicken Thighs, our profits are dedicated to supporting our
+          backyard chickens. By dining with us, you contribute to their romantic
+          endeavors. Please be patient, as our feathered friends need time to do
+          their thing—reproduce. It&apos;s all part of our commitment to
+          sustainable poultry practices.
+        </p>
+
+        <h2>Introducing Our Free Menu</h2>
+
+        <div className="menu">
+          <div className="menu-item">
+            <h3>Chicken Egg</h3>
+            <p>
+              A delicately cooked egg straight from the source. Breakfast and
+              dinner collide in this delectable treat.
+            </p>
+          </div>
+          <div className="menu-item">
+            <h3>Chicken Pancreas</h3>
+            <p>
+              Embark on a culinary adventure with our unconventional offering.
+              This dish is for the bold and daring!
+            </p>
+          </div>
+          <div className="menu-item">
+            <h3>Chicken Head</h3>
+            <p>
+              Make a lasting impression with this unique delicacy that&apos;ll
+              leave you clucking for more.
+            </p>
+          </div>
+          <div className="menu-item">
+            <h3>Chicken Kids</h3>
+            <p>
+              We recommend adopting these adorable chicks instead of eating
+              them. They&apos;re for cuddles, not consumption!
+            </p>
+          </div>
+        </div>
+
+        <h2>Special Sauce Extravaganza</h2>
+
+        <div className="special-sauces">
+          <div className="special-sauce">
+            <h3>Walnut Honey Skunk</h3>
+            <p>
+              A unique combination of walnut essence and the mysterious aroma of
+              a skunk.
+            </p>
+          </div>
+          <div className="special-sauce">
+            <h3>Not Really A Special Sauce (Weak Sauce)</h3>
+            <p>
+              A sauce that challenges the boundaries of mediocrity. Prepare for
+              a taste adventure!
+            </p>
+          </div>
+          <div className="special-sauce">
+            <h3>House Special Saliva</h3>
+            <p>
+              A magical sauce crafted from unicorn saliva. It&apos;s mystical,
+              it&apos;s enchanting—it&apos;s one of a kind.
+            </p>
+          </div>
+          <div className="special-sauce">
+            <h3>More Special Than You Sauce</h3>
+            <p>
+              A sauce that outshines even the most special of individuals. Brace
+              yourself for an extraordinary experience!
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
