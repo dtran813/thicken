7863ed2efba1bbb1d12a1206734d121f2b0c7328
dtran <dtran13899@gmail.com>
2023-05-28 08:18:18 -0600

    Add AboutUs and Commitment style, images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { FiInstagram, FiTwitter, FiFacebook } from 'react-icons/fi';
+import { aboutUs, slow } from '@/public';
 
 export default function Home() {
   return (
@@ -64,12 +66,16 @@ export default function Home() {
               <span className="block">Unleash Boldness,</span>
               <span className="mt-3 block">Embrace Slowness.</span>
             </p>
-            <p className="mb-10 text-base text-white/80 sm:text-xl">
-              At <span>Thicken Thighs</span>, you need to prepare for a culinary
-              adventure that is so slow, it will have you questioning the
-              meaning of haste in the most outrageously delicious way possible.
+            <p className="mb-10 text-lg text-white/80 sm:text-xl sm:leading-loose">
+              At{' '}
+              <span className="relative mr-1 inline-block before:absolute before:-inset-x-1 before:-inset-y-[1px] before:block before:-skew-y-3 before:bg-orange-400 hover:scale-105">
+                <span className="relative font-bold">Thicken Thighs</span>
+              </span>
+              , you need to prepare for a culinary adventure that is so slow, it
+              will have you questioning the meaning of haste in the most
+              outrageously delicious way possible.
             </p>
-            <button className="mt-4 w-fit rounded-bl-3xl rounded-br-md rounded-tl-md rounded-tr-3xl bg-orange-400  p-3 text-lg text-white sm:p-4">
+            <button className="mt-4 w-fit rounded-bl-md rounded-br-2xl rounded-tl-2xl rounded-tr-md bg-orange-400  p-3 text-lg text-white hover:scale-105 active:translate-y-1 sm:p-4">
               Make A Reservation
             </button>
           </div>
@@ -77,105 +83,131 @@ export default function Home() {
         </section>
       </header>
 
-      <section className="m-5">
-        <h2 className="mb-3 text-2xl">
-          Welcome to Thicken Thighs: Where Chickens Are Our Slow-Roasted
-          Superstars!
-        </h2>
+      <main className="bg-orange-200">
+        <div className="mx-auto max-w-7xl">
+          <section id="about-us" className="flex items-center">
+            <div className="flex h-full flex-1 flex-col">
+              <p className="text-lg italic text-black/80">- About Us -</p>
+              <h2 className="mt-9 text-6xl font-semibold">
+                Welcome to{' '}
+                <span className="relative mr-1 inline-block before:absolute before:-inset-x-1 before:-inset-y-[1px] before:block before:-skew-y-3 before:bg-orange-400 hover:scale-105">
+                  <span className="relative font-semibold">Thicken Thighs</span>
+                </span>
+                : Where Chickens Are Our Superstars!
+              </h2>
+              <p className="mx-auto my-10 text-lg leading-loose text-black/80">
+                At Thicken Thighs, we take pride in being the premier slow food
+                restaurant in the industry. Our commitment to slowness and
+                chicken-centric cuisine sets us apart. With the quality come
+                from the slowest staffs and thickest chickens, we&apos;re here
+                to serve up a memorable dining experience that will leave you
+                clucking for more!
+              </p>
+            </div>
 
-        <p className="mb-5">
-          At Thicken Thighs, we take pride in being the premier slow food
-          restaurant in the industry. Our commitment to slowness and
-          chicken-centric cuisine sets us apart. With a touch of sarcasm and a
-          dash of humor, we&apos;re here to serve up a memorable dining
-          experience that will leave you clucking for more!
-        </p>
+            <div className="flex flex-1">
+              <Image src={aboutUs} alt="Inside a restaurant" />
+            </div>
+          </section>
 
-        <h2 className="mb-3 text-2xl">Our Commitment to Slowness</h2>
+          <section id="about-us" className="flex flex-row-reverse items-center">
+            <div className="flex h-full flex-1 flex-col">
+              <p className="text-lg italic text-black/80">- Commitment -</p>
+              <h2 className="mt-9 text-6xl font-semibold">
+                Our Commitment to Slowness
+              </h2>
+              <p className="mx-auto my-10 text-lg leading-loose text-black/80">
+                We&apos;ve mastered the art of slow cooking, taking our time to
+                craft dishes that burst with amazing flavors. We&apos;re not
+                just slow, we&apos;re award-winningly slow! Join us on a
+                culinary adventure where every bite is worth the wait.
+              </p>
+            </div>
 
-        <p className="mb-5">
-          We&apos;ve mastered the art of slow cooking, taking our time to craft
-          dishes that burst with amazing flavors. We&apos;re not just slow,
-          we&apos;re award-winningly slow! Join us on a culinary adventure where
-          every bite is worth the wait.
-        </p>
+            <div className="flex flex-1">
+              <Image src={slow} alt="Inside a restaurant" />
+            </div>
+          </section>
 
-        <h2 className="mb-3 text-2xl">Chicken Reproduction Fund</h2>
+          <section>
+            <h2 className="mb-3 text-2xl">Chicken Reproduction Fund</h2>
 
-        <p className="mb-5">
-          At Thicken Thighs, our profits are dedicated to supporting our
-          backyard chickens. By dining with us, you contribute to their romantic
-          endeavors. Please be patient, as our feathered friends need time to do
-          their thing—reproduce. It&apos;s all part of our commitment to
-          sustainable poultry practices.
-        </p>
-
-        <h2 className="mb-3 text-2xl">Introducing Our Free Menu</h2>
-
-        <div className="flex flex-wrap justify-between">
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3 className="mt-0 text-xl">Chicken Egg</h3>
             <p className="mb-5">
-              A delicately cooked egg straight from the source. Breakfast and
-              dinner collide in this delectable treat.
+              At Thicken Thighs, our profits are dedicated to supporting our
+              backyard chickens. By dining with us, you contribute to their
+              romantic endeavors. Please be patient, as our feathered friends
+              need time to do their thing—reproduce. It&apos;s all part of our
+              commitment to sustainable poultry practices.
             </p>
-          </div>
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3 className="mt-0 text-xl">Chicken Pancreas</h3>
-            <p className="mb-5">
-              Embark on a culinary adventure with our unconventional offering.
-              This dish is for the bold and daring!
-            </p>
-          </div>
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3 className="mt-0 text-xl">Chicken Head</h3>
-            <p className="mb-5">
-              Make a lasting impression with this unique delicacy that&apos;ll
-              leave you clucking for more.
-            </p>
-          </div>
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3 className="mt-0 text-xl">Chicken Kids</h3>
-            <p className="mb-5">
-              We recommend adopting these adorable chicks instead of eating
-              them. They&apos;re for cuddles, not consumption!
-            </p>
-          </div>
+
+            <h2 className="mb-3 text-2xl">Introducing Our Free Menu</h2>
+
+            <div className="flex flex-wrap justify-between">
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3 className="mt-0 text-xl">Chicken Egg</h3>
+                <p className="mb-5">
+                  A delicately cooked egg straight from the source. Breakfast
+                  and dinner collide in this delectable treat.
+                </p>
+              </div>
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3 className="mt-0 text-xl">Chicken Pancreas</h3>
+                <p className="mb-5">
+                  Embark on a culinary adventure with our unconventional
+                  offering. This dish is for the bold and daring!
+                </p>
+              </div>
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3 className="mt-0 text-xl">Chicken Head</h3>
+                <p className="mb-5">
+                  Make a lasting impression with this unique delicacy
+                  that&apos;ll leave you clucking for more.
+                </p>
+              </div>
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3 className="mt-0 text-xl">Chicken Kids</h3>
+                <p className="mb-5">
+                  We recommend adopting these adorable chicks instead of eating
+                  them. They&apos;re for cuddles, not consumption!
+                </p>
+              </div>
+            </div>
+
+            <h2 className="mb-3 text-2xl">Special Sauce Extravaganza</h2>
+
+            <div className="flex flex-wrap justify-between">
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3>Walnut Honey Skunk</h3>
+                <p className="mb-5">
+                  A unique combination of walnut essence and the mysterious
+                  aroma of a skunk.
+                </p>
+              </div>
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3>Not Really A Special Sauce (Weak Sauce)</h3>
+                <p className="mb-5">
+                  A sauce that challenges the boundaries of mediocrity. Prepare
+                  for a taste adventure!
+                </p>
+              </div>
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3>House Special Saliva</h3>
+                <p className="mb-5">
+                  A magical sauce crafted from unicorn saliva. It&apos;s
+                  mystical, it&apos;s enchanting—it&apos;s one of a kind.
+                </p>
+              </div>
+              <div className="mb-5 w-80 bg-gray-200 p-5">
+                <h3>More Special Than You Sauce</h3>
+                <p className="mb-5">
+                  A sauce that outshines even the most special of individuals.
+                  Brace yourself for an extraordinary experience!
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
-
-        <h2 className="mb-3 text-2xl">Special Sauce Extravaganza</h2>
-
-        <div className="flex flex-wrap justify-between">
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3>Walnut Honey Skunk</h3>
-            <p className="mb-5">
-              A unique combination of walnut essence and the mysterious aroma of
-              a skunk.
-            </p>
-          </div>
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3>Not Really A Special Sauce (Weak Sauce)</h3>
-            <p className="mb-5">
-              A sauce that challenges the boundaries of mediocrity. Prepare for
-              a taste adventure!
-            </p>
-          </div>
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3>House Special Saliva</h3>
-            <p className="mb-5">
-              A magical sauce crafted from unicorn saliva. It&apos;s mystical,
-              it&apos;s enchanting—it&apos;s one of a kind.
-            </p>
-          </div>
-          <div className="mb-5 w-80 bg-gray-200 p-5">
-            <h3>More Special Than You Sauce</h3>
-            <p className="mb-5">
-              A sauce that outshines even the most special of individuals. Brace
-              yourself for an extraordinary experience!
-            </p>
-          </div>
-        </div>
-      </section>
+      </main>
     </>
   );
 }
---
+++ public/index.ts
@@ -0,0 +1,4 @@
+import aboutUs from './aboutUs.jpg';
+import slow from './slow.jpg';
+
+export { aboutUs, slow };
