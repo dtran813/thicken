c3188e9dadf0fcb86b0ab6c13ccaf4f5152bfde3
dtran <dtran13899@gmail.com>
2023-05-30 13:42:11 -0600

    Add images for sauces info

--- public/index.ts
+++ public/index.ts
@@ -1,9 +1,25 @@
 import aboutUs from './aboutUs.jpg';
-import slow from './slow.jpg';
 import chickens from './chickens.jpg';
+import slow from './slow.jpg';
 
-import egg from './free-menu/egg.jpg';
 import chickenHeart from './free-menu/chicken-heart.jpg';
 import chickenKids from './free-menu/chicken-kids.jpg';
+import egg from './free-menu/egg.jpg';
 
-export { aboutUs, slow, chickens, egg, chickenHeart, chickenKids };
+import saliva from './sauces/saliva.jpg';
+import special from './sauces/special.jpg';
+import walnut_honey from './sauces/walnut_honey.jpg';
+import weak_sauce from './sauces/weak_sauce.jpg';
+
+export {
+  aboutUs,
+  slow,
+  chickens,
+  egg,
+  chickenHeart,
+  chickenKids,
+  walnut_honey,
+  weak_sauce,
+  saliva,
+  special,
+};
