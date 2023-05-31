3471dac058f2bfaf42f2667c018f8f8efbf4905d
dtran <dtran13899@gmail.com>
2023-05-30 21:03:58 -0600

    Create Sauces section and refactor current code

---
+++ app/components/sauces/page.tsx
@@ -0,0 +1,26 @@
+import { saucesInfo } from '@/app/constants';
+import SauceInfo from './SauceInfo';
+
+const Sauces = () => {
+  return (
+    <section
+      id="sauces"
+      className="mt-20 border-t-2 border-dashed border-orange-600 pt-10"
+    >
+      <h2 className="mb-3 text-center text-6xl font-semibold">
+        Special Sauce Extravaganza
+      </h2>
+      <p className="mt-3 text-center text-xl leading-loose text-black/70">
+        Ketchup? Honey Mustard? BBQ? Boring. Let&apos;s see what we offer.
+      </p>
+
+      <div className="mt-10 flex flex-wrap justify-center gap-y-6 xl:gap-x-6">
+        {saucesInfo.map(({ name, desc, imgSrc }) => (
+          <SauceInfo key={name} imgSrc={imgSrc} name={name} desc={desc} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Sauces;
--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { CiWarning } from 'react-icons/ci';
 import { FiFacebook, FiInstagram, FiTwitter } from 'react-icons/fi';
-import SauceInfo from './components/SauceInfo';
-import { saucesInfo } from './constants';
+import Sauces from './components/sauces/page';
 
 export default function Home() {
   return (
@@ -267,23 +266,7 @@ export default function Home() {
             </div>
           </section>
 
-          <section
-            id="sauces"
-            className="mt-20 border-t-2 border-dashed border-orange-600 pt-10"
-          >
-            <h2 className="mb-3 text-center text-6xl font-semibold">
-              Special Sauce Extravaganza
-            </h2>
-            <p className="mt-3 text-center text-xl leading-loose text-black/70">
-              Ketchup? Honey Mustard? BBQ? Boring. Let&apos;s see what we offer.
-            </p>
-
-            <div className="mt-10 flex flex-wrap justify-center gap-y-6 xl:gap-x-6">
-              {saucesInfo.map(({ name, desc, imgSrc }) => (
-                <SauceInfo key={name} imgSrc={imgSrc} name={name} desc={desc} />
-              ))}
-            </div>
-          </section>
+          <Sauces />
 
           <section className="mt-20 border-t-2 border-dashed border-orange-600 pt-10"></section>
         </div>
