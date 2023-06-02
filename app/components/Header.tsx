6ff29da64582d2380d330dbc173dbfd048217858
dtran <dtran13899@gmail.com>
2023-06-01 18:04:45 -0600

    Add button to open mobile nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,22 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 import { navLinks } from '../constants';
 import SocialLinks from './SocialLinks';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="sticky left-0 top-0 z-10">
-      <div className="flex h-20 items-center bg-orange-200/75 p-5">
-        <div className="flex flex-1 justify-start">
+      <div className="flex h-20 items-center justify-between bg-orange-200/75 p-5">
+        <div className="flex justify-start xl:basis-1/3">
           <h1 className="text-2xl font-bold">
             <Link href="/">Thicken Thighs</Link>
           </h1>
         </div>
 
-        <nav className="flex flex-1 justify-center">
+        <nav className="hidden justify-center lg:flex xl:basis-1/3">
           <ul className="flex w-full list-none justify-between">
             {navLinks.map((link) => (
               <li key={link.label} className="inline-block">
@@ -24,9 +28,34 @@ const Header = () => {
           </ul>
         </nav>
 
-        <div className="flex flex-1 justify-end">
+        <div className="hidden justify-end lg:flex xl:basis-1/3">
           <SocialLinks />
         </div>
+
+        <button
+          className="block transition-all duration-500 ease-out lg:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div
+            className={`mb-1 h-[3px] w-[30px] bg-white transition duration-500 ease-out ${
+              isMenuOpen
+                ? 'translate-x-[6px] translate-y-[8px] rotate-[225deg]'
+                : ''
+            } `}
+          />
+          <div
+            className={`mb-1 h-[3px] w-[30px] bg-white transition-all duration-500 ease-out ${
+              isMenuOpen ? 'rotate-180 opacity-0' : ''
+            }`}
+          />
+          <div
+            className={`mb-1 h-[3px] w-[30px] bg-white transition duration-500 ease-out ${
+              isMenuOpen
+                ? '-translate-y-[6px] translate-x-[6px] rotate-[135deg]'
+                : ''
+            }`}
+          />
+        </button>
       </div>
     </header>
   );
