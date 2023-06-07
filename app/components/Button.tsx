34027b882a28e0566ef5ef57e3a66044486b05da
dtran <dtran13899@gmail.com>
2023-06-07 14:15:32 -0600

    Modify props of Button component to handle onClick and rounded border

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,23 @@
 interface ButtonProps {
   children: React.ReactNode;
+  roundedBorder?: boolean;
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  roundedBorder = true,
+  onClick,
+}) => {
   return (
-    <button className="mt-4 w-fit rounded-bl-md rounded-br-2xl rounded-tl-2xl rounded-tr-md bg-orange-400 px-7 py-4 text-lg text-white/90 shadow-md transition hover:scale-105 active:translate-y-1">
+    <button
+      className={`mt-4 w-fit bg-orange-400 px-7 py-4 text-lg text-white/90 shadow-md transition hover:scale-105 active:translate-y-1 ${
+        roundedBorder
+          ? 'rounded-bl-md rounded-br-2xl rounded-tl-2xl rounded-tr-md'
+          : 'rounded-md'
+      }`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
