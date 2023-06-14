6ef1ee3a6122aaa6d135443680a06469b3be16d1
dtran <dtran13899@gmail.com>
2023-06-13 21:56:17 -0600

    Add responsiveness for AboutUs page

--- app/aboutUs/AboutUsClient.tsx
+++ app/aboutUs/AboutUsClient.tsx
@@ -6,16 +6,20 @@ const AboutUsClient = () => {
   return (
     <div className="relative bg-orange-200">
       <div className="mx-auto max-w-screen-xl pb-10">
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="flex-1">
-            <Image src={aboutUs} alt="Our story" />
+        <div className="flex flex-col items-center md:flex-row xl:h-[calc(100vh-5rem)]">
+          <div className="h-96 w-96 flex-1 p-5 md:h-auto md:w-auto">
+            <Image
+              src={aboutUs}
+              alt="Our story"
+              className="h-full w-full object-cover"
+            />
           </div>
 
-          <div className="mb-10 flex flex-1 flex-col px-5 py-10">
+          <div className="flex flex-1 flex-col px-5 py-5 lg:py-10">
             <div className="flex justify-center px-5 pb-5">
               <Image src={aboutUsLogo} alt="Products from chickens" />
             </div>
-            <p className="p-10 text-base text-black/70 sm:leading-loose lg:text-xl">
+            <p className="text-base text-black/70 sm:leading-loose lg:text-xl lg:leading-relaxed xl:p-5">
               Thicken Thighs: where chickens achieve superstar status and
               flavors burst with mouthwatering madness. We are a fearless team
               of culinary rebels, serving up slow-cooked marvels that will have
