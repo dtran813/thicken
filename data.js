fc1016f09a5de3e355db7fa5fd59382080e2f1e3
dtran <dtran13899@gmail.com>
2023-06-15 17:48:36 -0600

    Add category field to Menu model and data for chicken menu and dessert menu

--- data.js
+++ data.js
@@ -4,6 +4,7 @@
     price: 11.99,
     calories: '1280',
     description: 'beer-battered, parmesan, chipotle ranch, buttermilk ranch',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801218/thicken/menu/appetizer/Onion-Ring-Tower_kouhx2.jpg',
   },
@@ -13,6 +14,7 @@
     calories: '730',
     description:
       'tofu, green onions, wonton cup, sweet chili, spicy peanut vinaigrette',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Chicken_Lettuce_Wraps_lndeim.jpg',
   },
@@ -22,6 +24,7 @@
     calories: '770 - 1010',
     description:
       'choose up to two sauces: buffalo • whiskey black pepper • bbq • korean chili garlic • lemon pepper dry rub',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/YH-Boneless-Korean-Wings_vrraoi.jpg',
   },
@@ -31,6 +34,7 @@
     calories: '940 - 1230',
     description:
       'choose up to two sauces: buffalo • whiskey black pepper • bbq • korean chili garlic • lemon pepper dry rub',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801216/thicken/menu/appetizer/TraditionalWings_jbs5ct.jpg',
   },
@@ -40,6 +44,7 @@
     calories: '750 - 1010',
     description:
       'choose up to two sauces: buffalo • whiskey black pepper • bbq • korean chili garlic • lemon pepper dry rub',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Korea-Gardein-Wings_ubtqbz.jpg',
   },
@@ -48,6 +53,7 @@
     price: 6.99,
     calories: '500',
     description: 'white truffle oil, parmesan, thyme, parsley',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/truffle-fries-thick_yucdsr.jpg',
   },
@@ -56,6 +62,7 @@
     price: 6.59,
     calories: '650',
     description: 'sweet potato with some potatoes that may not sweet',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Sweet-Potato-Fries_yb1ou7.jpg',
   },
@@ -64,6 +71,7 @@
     price: 11.99,
     calories: '1950',
     description: 'honey hot sauce, horseradish aïoli',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Wisconsin-Fried-Cheese-Curds_ajgebh.jpg',
   },
@@ -72,7 +80,92 @@
     price: 12.49,
     calories: '750 - 1230',
     description: 'maple dijon, buttermilk ranch, house fries',
+    category: 'appetizer',
     imgUrl:
       'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Fried-Chicken-Tenders-Appetizer_vin4pb.jpg',
+  },
+];
+
+[
+  {
+    name: 'Maui Pineapple Chicken',
+    price: 21.99,
+    calories: '1270',
+    description:
+      'grilled with pineapple, baby broccoli, jasmine rice, sweet soy ginger sauce',
+    category: 'main-chicken',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686871909/thicken/menu/main/Maui_Pineapple_Chicken_y5kqxp.jpg',
+  },
+  {
+    name: 'Cilantro Lime Chicken',
+    price: 21.99,
+    calories: '660',
+    description:
+      'grilled with roasted corn, radish, baby tomato relish, stir-fried cauliflower rice & red quinoa, cilantro',
+    category: 'main-chicken',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686871909/thicken/menu/main/cilantro_lime_chicken_azlsvl.jpg',
+  },
+  {
+    name: 'Orange Chicken',
+    price: 18.99,
+    calories: 'bok choy, baby corn, orange glaze, jasmine rice',
+    description: '1340',
+    category: 'main-chicken',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686871909/thicken/menu/main/Orange-Chicken_q6x3mo.jpg',
+  },
+  {
+    name: 'Nashville Hot Chicken & Pancakes',
+    price: 19.99,
+    calories: '1540',
+    description:
+      'sweet potato pancakes, pickles, ranch dressing, honey hot sauce',
+    category: 'main-chicken',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686871909/thicken/menu/main/Nashville_Hot_Chicken_and_Pancakes_ztx5gy.jpg',
+  },
+];
+
+[
+  {
+    name: 'Brookie',
+    price: 8.99,
+    calories: '1160',
+    description:
+      'cookie dough fudge brownie, chocolate & caramel sauces, vanilla ice cream',
+    category: 'dessert',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686872364/thicken/menu/dessert/Brookie_havbli.jpg',
+  },
+  {
+    name: 'Chocolate Fudge Cake',
+    price: 9.99,
+    calories: '860',
+    description:
+      'three-layer dark chocolate, fudge frosting, chocolate chips, marbled chocolate sauce',
+    category: 'dessert',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686872364/thicken/menu/dessert/Chocolate-Fudge-Cake_ard24x.jpg',
+  },
+  {
+    name: 'Bread Pudding',
+    price: 9.99,
+    calories: '680',
+    description:
+      'vanilla bean & caramel sauces, vanilla ice cream, powdered sugar',
+    category: 'dessert',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686872364/thicken/menu/dessert/Bread-Pudding_taz4aw.jpg',
+  },
+  {
+    name: 'Mini Cheesecake',
+    price: 6.59,
+    calories: '400',
+    description: 'caramelized crispy sugar topping',
+    category: 'dessert',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686872364/thicken/menu/dessert/MiniCheeseCake_gbizvs.jpg',
   },
 ];
