9cb2fe39a8a5ec0c05044445db7448a00a03064a
dtran <dtran13899@gmail.com>
2023-06-14 22:17:14 -0600

    Data for appetizer menus

---
+++ data.js
@@ -0,0 +1,78 @@
+[
+  {
+    name: 'Onion Ring Tower',
+    price: 11.99,
+    calories: '1280',
+    description: 'beer-battered, parmesan, chipotle ranch, buttermilk ranch',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801218/thicken/menu/appetizer/Onion-Ring-Tower_kouhx2.jpg',
+  },
+  {
+    name: 'Chicken Lettuce Wraps',
+    price: 12.39,
+    calories: '730',
+    description:
+      'tofu, green onions, wonton cup, sweet chili, spicy peanut vinaigrette',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Chicken_Lettuce_Wraps_lndeim.jpg',
+  },
+  {
+    name: 'Boneless Wings',
+    price: 13.99,
+    calories: '770 - 1010',
+    description:
+      'choose up to two sauces: buffalo • whiskey black pepper • bbq • korean chili garlic • lemon pepper dry rub',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/YH-Boneless-Korean-Wings_vrraoi.jpg',
+  },
+  {
+    name: '10 Crispy Traditional Wings',
+    price: 14.59,
+    calories: '940 - 1230',
+    description:
+      'choose up to two sauces: buffalo • whiskey black pepper • bbq • korean chili garlic • lemon pepper dry rub',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801216/thicken/menu/appetizer/TraditionalWings_jbs5ct.jpg',
+  },
+  {
+    name: 'Garden Wings',
+    price: 13.99,
+    calories: '750 - 1010',
+    description:
+      'choose up to two sauces: buffalo • whiskey black pepper • bbq • korean chili garlic • lemon pepper dry rub',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Korea-Gardein-Wings_ubtqbz.jpg',
+  },
+  {
+    name: 'Parm Truffle Fries',
+    price: 6.99,
+    calories: '500',
+    description: 'white truffle oil, parmesan, thyme, parsley',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/truffle-fries-thick_yucdsr.jpg',
+  },
+  {
+    name: 'Sweet Potato Fries',
+    price: 6.59,
+    calories: '650',
+    description: 'sweet potato with some potatoes that may not sweet',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Sweet-Potato-Fries_yb1ou7.jpg',
+  },
+  {
+    name: 'Wisconsin Fried Cheese Curds',
+    price: 11.99,
+    calories: '1950',
+    description: 'honey hot sauce, horseradish aïoli',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Wisconsin-Fried-Cheese-Curds_ajgebh.jpg',
+  },
+  {
+    name: 'Hand-battered Chicken Tenders',
+    price: 12.49,
+    calories: '750 - 1230',
+    description: 'maple dijon, buttermilk ranch, house fries',
+    imgUrl:
+      'https://res.cloudinary.com/dxmy0qse5/image/upload/v1686801217/thicken/menu/appetizer/Fried-Chicken-Tenders-Appetizer_vin4pb.jpg',
+  },
+];
