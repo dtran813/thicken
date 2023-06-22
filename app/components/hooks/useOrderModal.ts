d406b2358e4fddec4555b71a61e796463e79dcc6
dtran <dtran13899@gmail.com>
2023-06-21 20:54:51 -0600

    Add zustand and create new hook

---
+++ app/components/hooks/useOrderModal.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+interface OrderModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useOrderModal = create<OrderModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useOrderModal;
