3b0028d79630e6b827a5ac261044181bc9e4479c
dtran <dtran13899@gmail.com>
2023-07-14 22:47:24 -0600

    Add validation for ContactUsForm

--- app/contactUs/ContactUsForm.tsx
+++ app/contactUs/ContactUsForm.tsx
@@ -1,15 +1,51 @@
+'use client';
+
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Button from '../components/Button';
 import Input from '../components/inputs/Input';
 
 const ContactUsForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log({ data });
+  };
+
   return (
     <div className="flex justify-center p-5">
-      <form className="flex w-full max-w-4xl flex-col items-stretch">
-        <Input id="fullName" label="Full Name" required />
+      <form
+        className="flex w-full max-w-4xl flex-col items-stretch"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input
+          id="fullName"
+          label="Full Name"
+          register={register}
+          errors={errors}
+          required
+        />
 
-        <Input id="email" label="Email Address" required />
+        <Input
+          id="email"
+          label="Email Address"
+          type="email"
+          register={register}
+          errors={errors}
+          required
+        />
 
-        <Input id="message" label="Message" required isTextarea />
+        <Input
+          id="message"
+          label="Message"
+          register={register}
+          errors={errors}
+          required
+          isTextarea
+        />
 
         <Button className="mt-4 bg-orange-400 px-8 py-4 text-lg font-medium text-white/90">
           Submit
